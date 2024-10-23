2381ee6b30053fcd430770666cf3a7ec19d48ade
Peter <kompaszbence@gmail.com>
2024-10-23 19:53:30 +0200

    feat: add builders data

--- packages/nextjs/app/builders/page.tsx
+++ packages/nextjs/app/builders/page.tsx
@@ -2,16 +2,37 @@
 
 import { useState } from "react";
 import type { NextPage } from "next";
+import { FaExternalLinkAlt } from "react-icons/fa";
 import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 type Builder = {
   name: string;
-  profileHref: string;
-  addressOrEns: string;
+  address: `0x${string}`;
 };
 
 const Home: NextPage = () => {
-  const [builders] = useState([] as Builder[]);
+  const [builders] = useState([
+    {
+      name: "Abdulyekeen Lukman",
+      address: "0x186a761645f2A264ad0A655Fb632Ca99150803A9",
+    },
+    {
+      name: "Favvie Kenpachi",
+      address: "0x21Be2291f91EA2A1d1EB65DbBea2dA8886Ad7a3E",
+    },
+    {
+      name: "Bello Abraham",
+      address: "0x28482B1279E442f49eE76351801232D58f341CB9",
+    },
+    {
+      name: "Samson Aderonmu",
+      address: "0x62CeF3Ca8b52a9C69a17236CA2c56Cdb7a383E8e",
+    },
+    {
+      name: "Michael Ojekunle",
+      address: "0x7429CbD5eD20736645723E972bE60B7F6BF5959c",
+    },
+  ] as Builder[]);
 
   const { data: checkedInCounter } = useScaffoldReadContract({
     contractName: "BatchRegistry",
@@ -44,12 +65,21 @@ const Home: NextPage = () => {
                 </thead>
                 <tbody>
                   {builders.map((builder, id) => (
-                    <tr key={id}>
-                      <th>{id}</th>
-                      <td>
-                        <a href={builder.profileHref}>{builder.name}</a>
+                    <tr key={id} className="items-center">
+                      <th>{id + 1}</th>
+                      <td className="underline text-lg">
+                        <a href={`/builders/${builder.address}`}>{builder.name}</a>
                       </td>
-                      <td>{builder.addressOrEns}</td>
+                      <td className="flex flex-nowrap items-center gap-1 content-center h-100">
+                        <a
+                          href={`https://optimistic.etherscan.io/address/${builder.address}`}
+                          target="_blank"
+                          className="underline"
+                        >
+                          {builder.address}
+                        </a>
+                        <FaExternalLinkAlt size="12" />
+                      </td>
                     </tr>
                   ))}
                 </tbody>
