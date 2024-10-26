3157f286bb908023274976fb452b1644bb16a73a
Peter <kompaszbence@gmail.com>
2024-10-26 21:14:45 +0200

    chore: add missing builder names

--- packages/nextjs/app/builders/constants.ts
+++ packages/nextjs/app/builders/constants.ts
@@ -1,9 +1,12 @@
 const BUILDERS = {
   "0x186a761645f2a264ad0a655fb632ca99150803a9": "Abdulyekeen Lukman",
   "0x21be2291f91ea2a1d1eb65dbbea2da8886ad7a3e": "Favvie Kenpachi",
+  "0x26bfbd8ed2b302ec2c2b6f063c4caf7abcb062e0": "Peter Kompasz",
   "0x28482b1279e442f49ee76351801232d58f341cb9": "Bello Abraham",
+  "0x549dd44dea0127254da32b0c4e79ae3d2cb8b94d": "Samson Aderonmu",
   "0x62cef3ca8b52a9c69a17236ca2c56cdb7a383e8e": "Samson Aderonmu",
   "0x7429cbd5ed20736645723e972be60b7f6bf5959c": "Michael Ojekunle",
+  "0xe9ad7d1c2069e0fa9b5852adc77c9196651bb8b8": "0xe9Ad",
 } as { [key: string]: string };
 
 export { BUILDERS };
