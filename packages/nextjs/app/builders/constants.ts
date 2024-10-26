6a2adf16523eef2db63ee6c94dfa2c3ee9efb1af
Peter <kompaszbence@gmail.com>
2024-10-26 20:28:08 +0200

    chore: builder names

---
+++ packages/nextjs/app/builders/constants.ts
@@ -0,0 +1,9 @@
+const BUILDERS = {
+  "0x186a761645f2a264ad0a655fb632ca99150803a9": "Abdulyekeen Lukman",
+  "0x21be2291f91ea2a1d1eb65dbbea2da8886ad7a3e": "Favvie Kenpachi",
+  "0x28482b1279e442f49ee76351801232d58f341cb9": "Bello Abraham",
+  "0x62cef3ca8b52a9c69a17236ca2c56cdb7a383e8e": "Samson Aderonmu",
+  "0x7429cbd5ed20736645723e972be60b7f6bf5959c": "Michael Ojekunle",
+} as { [key: string]: string };
+
+export { BUILDERS };
